c8f9b013ee90c29907d18ed2175b44bb85edbe24
Jihong <njutangjihong@gmail.com>
2019-07-19 16:48:20 -0400

    Update app.R

--- app.R
+++ app.R
@@ -1,4 +1,7 @@
 library(shiny)
+library(flowCore)
+library(tidyverse)
+library(openxlsx)
 
 # Define UI for data upload app ----
 ui <- fluidPage(
@@ -22,19 +25,11 @@ ui <- fluidPage(
       # Input: Checkbox if file has header ----
       checkboxInput("header", "Header", TRUE),
       
-      # Input: Select separator ----
-      radioButtons("sep", "Separator",
-                   choices = c(Comma = ",",
-                               Semicolon = ";",
-                               Tab = "\t"),
-                   selected = ","),
-      
-      # Input: Select quotes ----
-      radioButtons("quote", "Quote",
-                   choices = c(None = "",
-                               "Double Quote" = '"',
-                               "Single Quote" = "'"),
-                   selected = '"'),
+      # Input: Select input file type ----
+      radioButtons("intype", "File Type",
+                   choices = c(Index = "index",
+                               Other = "other"),
+                   selected = "index"),
       
       # Horizontal line ----
       tags$hr(),
@@ -43,15 +38,37 @@ ui <- fluidPage(
       radioButtons("disp", "Display",
                    choices = c(Head = "head",
                                All = "all"),
-                   selected = "head")
+                   selected = "head"),
       
+      tags$hr(),
+      
+      tags$br(),
+      
+      tags$br(),
+      
+      tags$br(),
+      
+      tags$hr(),
+      
+      # Input: Select output file format ----
+      radioButtons("outtype", "Save Format",
+                   choices = c(Excel = "excel",
+                               Text = "txt",
+                               CSV = "csv"),
+                   selected = "excel"),
+      tags$hr(),
+      # Button
+      downloadButton("downloadData", "Download")
     ),
     
     # Main panel for displaying outputs ----
     mainPanel(
       
-      # Output: Data file ----
-      tableOutput("contents")
+      tabsetPanel(
+        tabPanel("Table Show", 
+                 dataTableOutput("tableshow")),
+        tabPanel("Other fun", "contents2")
+      )
       
     )
     
@@ -61,28 +78,45 @@ ui <- fluidPage(
 # Define server logic to read selected file ----
 server <- function(input, output) {
   
-  output$contents <- renderTable({
+  datainput <- reactive({
+    fcm <- read.FCS(input$file1$datapath)
+    fcm <- as.data.frame((exprs(fcm)))
+  })
+  
+  output$tableshow <- renderDataTable({
     
     # input$file1 will be NULL initially. After the user selects
     # and uploads a file, head of that data file by default,
     # or all rows if selected, will be shown.
-    
     req(input$file1)
-    
-    df <- read.csv(input$file1$datapath,
-                   header = input$header,
-                   sep = input$sep,
-                   quote = input$quote)
-    
     if(input$disp == "head") {
-      return(head(df))
+      return(head(datainput()))
+    }else if(input$disp == "all") {
+      return(datainput())
     }
-    else {
-      return(df)
-    }
-    
   })
   
+  # Downloadable csv of selected dataset ----
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      if(input$outtype == "excel"){
+        sub("fcs", "xlsx", input$file1$name)
+      }else if(input$outtype == "txt"){
+        sub("fcs", "txt", input$file1$name)
+      }else if(input$outtype == "csv"){
+        sub("fcs", "csv", input$file1$name)
+      }
+    },
+    content = function(file) {
+      if(input$outtype == "excel"){
+        write.xlsx(datainput(), file)
+      }else if(input$outtype == "txt"){
+        write_delim(datainput(), file)
+      }else if(input$outtype == "csv"){
+        write_csv(datainput(), file)
+      }
+    }
+  )
 }
 
 # Create Shiny app ----
